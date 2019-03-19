e0c8c7625f8ac7e660f96d88aa7f8c7ad6a1592f
comarasj <comarasj@mail.uc.edu>
2019-03-19 17:47:37 -0400

    added a settings.py to inherit .env file keys

--- artist.py
+++ artist.py
@@ -1,40 +1,36 @@
 import sys
 import spotipy
 import spotipy.util as util
-# import setup
 import os
 import subprocess
-
+import settings as settings
 
 scope = 'user-top-read user-read-private playlist-modify-private playlist-modify-public'
 
-# if len(sys.argv) > 3:
-username = "Stephen Comaraata"
-username = "1255822034"
-playlist_name = "Test"
+playlist_name = "helloworld"
 playlist_description = "helloworld"
-#     playlist_id = "2018"
-#     track_ids = ""
-# else:
-#     print("Usage: %s username" % (sys.argv[0],))
-#     sys.exit()
 
-token = util.prompt_for_user_token(username,scope,client_id='a6c7402240df4a8486ec522cb82a2ba7',client_secret='5d142ecd5cfc4308852aaa5e477115e0',redirect_uri='http://localhost/')
+artists = ["Owl City", "Imagine Dragons", "Adam Young"]
+
+token = util.prompt_for_user_token(settings.user,scope,client_id=settings.client_id,client_secret=settings.client_secret,redirect_uri=settings.redirect_uri)
 spotifyObject = spotipy.Spotify(auth=token)
+
+def get_artist_id(name):
+    # Popularity greater than 30
+    artist = spotifyObject.search(q='artist:' + name, type='artist')
+    artist_id = []
+    for i in artist['artists']['items']:
+        if int(i['popularity']) > 30:
+            artist_id.append(i['id'])
+    return artist_id
 
 
 if token:
     sp = spotipy.Spotify(auth=token)
     sp.trace = False
-    # artists = ['Owl City']
-    # artists = getArtistIds(artists)
-    # print(artists)
-    
-    
-    playlists = sp.user_playlist_create(username, playlist_name, public=False)
-    
-
-
+    for i in artists:
+        get_artist_id(i)
+    # playlists = sp.user_playlist_create(settings.user, playlist_name, public=False)
 
 else:
-    print("Can't get token for", username)
+    print("Can't get token for", settings.user)
